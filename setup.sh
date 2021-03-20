067a9e6f2eda4d8ad89d15ec9f7242d3d92ad5e3
rodneyshupe <rodney@shupe.ca>
2021-03-20 01:05:26 -0700

    Add setup file.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+# TODO: check if pyenv is installed if so install latest python3
+#pyenv install 3.7.10
+python -m pip install --upgrade pip
+python -m pip install rst2pdf
+
+# TODO: need to make this cross platform.
+brew install calibre
